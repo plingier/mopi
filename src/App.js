8d84d76dfb292cfe827abf086a96988f2ce77c67
Lucas Vander Beken <lvb@teletask.be>
2018-09-15 23:12:20 +0200

    Working call to REST server

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import MopiHeader from './MopiHeader';
 
+// Define a minimum and maximum year (the max is the current year)
+const MIN_YEAR = 1990;
+const MAX_YEAR = (new Date()).getFullYear();
+
+// These are URL prefixes for the backdrop and poster images
+const BACKDROP_PATH_PREFIX = 'https://image.tmdb.org/t/p/w780';
+const POSTER_PATH_PREFIX = 'https://image.tmdb.org/t/p/w500';
+
 class App extends Component {
   componentDidMount() {
-    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c42d6110155d2e79d0d1fd378a0fe02e')
+    // Set initial state
+    this.setState({
+      randomMovie: {}
+    });
+
+    // Calculate a random year between MIN_YEAR and MAX_YEAR (excluded)
+    let randomYear = Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR) + MIN_YEAR);
+
+    // Fetch all movies from the random year
+    fetch('https://api.themoviedb.org/3/discover/movie?year=' + randomYear + '&api_key=c42d6110155d2e79d0d1fd378a0fe02e')
       .then(response => response.json())
       .then(data => {
+        // Now fetch a random page, based on the total_pages property returned by the previous call
         fetch('https://api.themoviedb.org/3/discover/movie?page=' + Math.floor((Math.random() * data.total_pages) + 1) + '&api_key=c42d6110155d2e79d0d1fd378a0fe02e')
           .then(response => response.json())
           .then(data => {
-            this.setState( data[Math.floor(Math.random() * data.length)] )
+            // Get a random movie from that page
+            let randomMovie = data.results[Math.floor(Math.random() * data.results.length)];
+
+            // Update our state
+            this.setState({randomMovie: randomMovie});
+
+            // This is just for debugging
+            console.log('Random movie: ', randomMovie);
+            console.log('Backdrop path: ', BACKDROP_PATH_PREFIX + randomMovie.backdrop_path);
+            console.log('Poster path: ', POSTER_PATH_PREFIX + randomMovie.poster_path);
           });
       });
   }
 
   render() {
     return (
-      <MopiHeader />
+      <div>
+        <MopiHeader />
+
+        <footer>Powered by <a href="https://www.tmdb.org/">The Movie DB</a></footer>
+      </div>
     );
   }
 }
