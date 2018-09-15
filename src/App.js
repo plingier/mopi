fad61f499e58ffe400b2daa17e12f43504f72c31
Lucas Vander Beken <lvb@teletask.be>
2018-09-15 23:23:12 +0200

    Allow only movies with at least 400 votes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import MopiHeader from './MopiHeader';
 const MIN_YEAR = 1990;
 const MAX_YEAR = (new Date()).getFullYear();
 
+// We only want movies with at least X votes
+const MIN_VOTES = 400;
+
 // These are URL prefixes for the backdrop and poster images
 const BACKDROP_PATH_PREFIX = 'https://image.tmdb.org/t/p/w780';
 const POSTER_PATH_PREFIX = 'https://image.tmdb.org/t/p/w500';
@@ -20,12 +23,13 @@ class App extends Component {
     // Calculate a random year between MIN_YEAR and MAX_YEAR (excluded)
     let randomYear = Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR) + MIN_YEAR);
 
-    // Fetch all movies from the random year
-    fetch('https://api.themoviedb.org/3/discover/movie?year=' + randomYear + '&api_key=c42d6110155d2e79d0d1fd378a0fe02e')
+    // Fetch all movies from the random year, with at least MIN_VOTES votes
+    let url = 'https://api.themoviedb.org/3/discover/movie?year=' + randomYear + '&vote_count.gte=' + MIN_VOTES + '&api_key=c42d6110155d2e79d0d1fd378a0fe02e';
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         // Now fetch a random page, based on the total_pages property returned by the previous call
-        fetch('https://api.themoviedb.org/3/discover/movie?page=' + Math.floor((Math.random() * data.total_pages) + 1) + '&api_key=c42d6110155d2e79d0d1fd378a0fe02e')
+        fetch(url + '&page=' + Math.floor((Math.random() * data.total_pages) + 1))
           .then(response => response.json())
           .then(data => {
             // Get a random movie from that page
