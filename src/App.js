69553937953bacdef518d6a479f6b5b10ff9b0d2
Lucas Vander Beken <lvb@teletask.be>
2018-09-15 20:03:35 +0200

    Adds a first implementation of a REST call

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=c42d6110155d2e79d0d1fd378a0fe02e')
+      .then(response => response.json())
+      .then(data => {
+        fetch('https://api.themoviedb.org/3/discover/movie?page=' + Math.floor((Math.random() * data.total_pages) + 1) + '&api_key=c42d6110155d2e79d0d1fd378a0fe02e')
+          .then(response => response.json())
+          .then(data => {
+            this.setState( data[Math.floor(Math.random() * data.length)] )
+          });
+      });
+  }
+
   render() {
     return (
       <div className="App">
